af9d12384cd1fdc01ce2d348d70fae6a78f1e29c
Eoin O'Brien <iameoinobrien@gmail.com>
2020-04-18 23:33:24 +0100

    Use Dublin Timezone for LastTram

--- src/i18n.js
+++ src/i18n.js
@@ -1,30 +1,31 @@
 import i18n from "i18next";
+import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
 i18n
-   .use(LanguageDetector)
-   .init(
-      {
-         resources: {},
-         debug: false,
-         detection: {
-            order: ['path', 'localStorage', 'navigator', 'htmlTag']
-         },
+    .use(LanguageDetector)
+    .use(Backend)
+    .init(
+        {
+            backend: {
+                loadPath: '/locales/{{lng}}/{{ns}}.json'
+            },
+            debug: true,
 
-         backend: {
-            loadPath: './locales/{{lng}}/{{ns}}.json'
-         },
+            detection: {
+                order: ['path', 'localStorage', 'navigator', 'htmlTag']
+            },
 
-         fallbackLng: "en",
-         whitelist: ["en", "ga"],
-         checkWhitelist: true,
+            fallbackLng: "en",
+            whitelist: ["en", "ga"],
+            checkWhitelist: true,
 
-         keySeparator: '.',
+            keySeparator: '.',
 
-         interpolation: {
-            escapeValue: false
-         }
-      }
-   );
+            interpolation: {
+                escapeValue: false
+            }
+        }
+    );
 
 export default i18n;
