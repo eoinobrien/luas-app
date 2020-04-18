dd67359ec9f5605b6172907a8d8651653dd0bad7
Eoin O'Brien <iameoinobrien@gmail.com>
2020-04-19 00:40:00 +0100

    rename help folder

--- src/components/ss/help.tsx
+++ src/components/ss/help.tsx
@@ -1,5 +1,5 @@
-import React from './node_modules/react';
-import { useTranslation } from './node_modules/react-i18next';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
 import './help.scss';
 
 const Help: React.FC<{}> = () => {
