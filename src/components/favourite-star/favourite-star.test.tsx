c2480c902c420493b14a695f1b46dfda7a025a2d
Eoin O'Brien <iameoinobrien@gmail.com>
2020-04-19 23:34:14 +0100

    add some tests to favourite star

--- src/components/favourite-star/favourite-star.test.tsx
+++ src/components/favourite-star/favourite-star.test.tsx
@@ -1,15 +1,36 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { shallow, mount } from 'enzyme';
 import FavouriteStar from './favourite-star';
 
 
-test('renders correctly', () => {
-   const favouriteClick = () => {
-      console.log("click");
-   }
+test('FavouriteStar > Matches snapshot', () => {
+    const mockFavouriteClick = jest.fn(x => console.log("clicked"));
 
-   const tree = renderer
-      .create(<FavouriteStar isFavourite={false} favouriteClick={favouriteClick} />)
-      .toJSON();
-   expect(tree).toMatchSnapshot();
+    let tree = renderer
+        .create(<FavouriteStar isFavourite={false} favouriteClick={mockFavouriteClick} name="station name" />)
+        .toJSON();
+    expect(tree).toMatchSnapshot();
+
+    tree = renderer
+        .create(<FavouriteStar isFavourite={true} favouriteClick={mockFavouriteClick} name="station name" />)
+        .toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+test('FavouriteStar > Styles are correct', () => {
+    let isFavourite = false;
+    const mockFavouriteClick = jest.fn(() => isFavourite = !isFavourite);
+
+    let star = mount(<FavouriteStar isFavourite={isFavourite} favouriteClick={mockFavouriteClick} name="station name" />);
+
+    expect(star.find('svg').prop('style')).toBeUndefined();
+    star.simulate('click');
+    expect(mockFavouriteClick.mock.calls.length).toBe(1);
+
+    star = mount(<FavouriteStar isFavourite={isFavourite} favouriteClick={mockFavouriteClick} name="station name" />);
+
+    expect(star.find('svg').prop('style')).toHaveProperty('color', '#fdd835');
+    star.simulate('click');
+    expect(mockFavouriteClick.mock.calls.length).toBe(2);
 });
