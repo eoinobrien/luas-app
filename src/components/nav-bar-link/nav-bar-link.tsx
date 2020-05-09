b819c93c8090bae02dbe37afb9956b2ab2bdca24
Eoin O'Brien <iameoinobrien@gmail.com>
2020-05-09 20:05:43 +0100

    scroll favourites bar

--- src/components/nav-bar-link/nav-bar-link.tsx
+++ src/components/nav-bar-link/nav-bar-link.tsx
@@ -3,18 +3,20 @@ import { Link, withRouter, RouteComponentProps } from 'react-router-dom';
 import './nav-bar-link.scss';
 
 interface NavBarLinkProps extends RouteComponentProps {
-   value: string;
-   to: string;
-   colour: string;
-   className: string;
+    value: string;
+    to: string;
+    colour: string;
+    className: string;
 }
 
 const NavBarLink: React.FC<NavBarLinkProps> = (props: NavBarLinkProps) => {
-   return (
-      <Link className={'navbar-link ' + props.className!} to={props.to} style={{ borderColor: props.colour }}>
-         {props.value}
-      </Link>
-   );
+    return (
+        <div className='navbar-link'>
+            <Link  className={props.className!} to={props.to} style={{ borderColor: props.colour }}>
+                {props.value}
+            </Link>
+        </div>
+    );
 }
 
 export default withRouter(NavBarLink);
