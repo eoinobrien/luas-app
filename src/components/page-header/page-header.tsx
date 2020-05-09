3cdcb802163cd3cc5d2e177aade8b82339836dc2
Eoin O'Brien <iameoinobrien@gmail.com>
2020-05-09 19:30:29 +0100

    Improve header spacing

--- src/components/page-header/page-header.tsx
+++ src/components/page-header/page-header.tsx
@@ -20,24 +20,30 @@ const PageHeader: React.FC<PageHeaderProps> = (props: PageHeaderProps) => {
     return (
         <header
             className="page-header"
-            style={{borderColor: props.color}}>
-            <Link
-                className="back-arrow"
-                aria-label="Go Back to the list of Stations"
-                to={'/'}>
-                <LeftArrow />
-            </Link>
-            <h1>
-                {(i18n.language === "ga" && props.headerTitleIrish) || props.headerTitle}
-                <span> {!props.loading && props.headerTitleIrish !== undefined && ((i18n.language === "ga" && props.headerTitle) || props.headerTitleIrish)}</span></h1>
+            style={{ borderColor: props.color }}>
+            <div>
+                <Link
+                    className="back-arrow"
+                    aria-label="Go Back to the list of Stations"
+                    to={'/'}>
+                    <LeftArrow />
+                </Link>
+            </div>
+            <div className="title">
+                <h1>
+                    {(i18n.language === "ga" && props.headerTitleIrish) || props.headerTitle}
+                    <span> {!props.loading && props.headerTitleIrish !== undefined && ((i18n.language === "ga" && props.headerTitle) || props.headerTitleIrish)}</span></h1>
+            </div>
 
             {props.favouriteClick !== undefined &&
                 props.isFavourite !== undefined &&
                 !props.loading &&
-                <FavouriteStar
-                    name={(i18n.language === "ga" && props.headerTitleIrish) || props.headerTitle}
-                    isFavourite={props.isFavourite ?? false}
-                    favouriteClick={props.favouriteClick} />}
+                <div>
+                    <FavouriteStar
+                        name={(i18n.language === "ga" && props.headerTitleIrish) || props.headerTitle}
+                        isFavourite={props.isFavourite ?? false}
+                        favouriteClick={props.favouriteClick} />
+                </div>}
         </header>
     );
 };
