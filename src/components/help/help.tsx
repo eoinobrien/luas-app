4afa01ba7e850980c58d6dca1d837a9235f713b0
Eoin O'Brien <eoobrien@microsoft.com>
2020-07-06 19:57:15 +0100

    Update help informaion

--- src/components/help/help.tsx
+++ src/components/help/help.tsx
@@ -16,7 +16,7 @@ const Help: React.FC<{}> = () => {
                 isFavourite={false} />
 
             <section className="settings">
-                <h2>Choose Language</h2>
+                <h2>Language/Teanga</h2>
 
                 <div className="language-selector">
                     <label className={i18n.language === "en" ? 'active' : ''}>
@@ -32,11 +32,21 @@ const Help: React.FC<{}> = () => {
             </section>
 
             <section>
-                <h1>Help</h1>
-                <p>You can visit a station by clicking on a station on the previous page.</p>
+                <h2>Feedback</h2>
+                <p>If you have any feedback, good or bad, I would love to hear it. Feel free to contact me at <a href="https://twitter.com/iameoinobrien">@iameoinobrien on Twitter</a></p>
+
+                <h2>Irish Translations</h2>
+                <p>If you have better Irish than me, and would like to help improve the translations in this app, I would love the help. Please reach out to me on <a href="https://twitter.com/iameoinobrien">Twitter</a> or <a href="https://github.com/eoinobrien/luas-app/issues">file an issue on Github</a></p>
 
                 <h2>About this app</h2>
-                <p>You can find the code for this site on <a href="https://github.com/eoinobrien/luas-app/">Github</a></p>
+                <p>This app was a lockdown project for me, <a href="https://eoinobrien.ie">Eoin O'Brien</a>, and is meant to serve as a simple, quick and reliable Luas application.</p>
+                <p>It is licensed under an MIT License you can find the code for this site on <a href="https://github.com/eoinobrien/luas-app/">Github</a>. If you find an issue please <a href="https://github.com/eoinobrien/luas-app/issues">file an issue</a>.</p>
+
+                <h2>Privacy</h2>
+                <p>We do not store any personal information about you while you use this app or the API that powers it.</p>
+                <p>We do store some non-personal information (e.g. station information and your favorites) in your browser to give you the best experience.</p>
+                <p>The app and API is hosted on Microsoft Azure, but we do not log any personal information about your calls, but we do log some information like which station was queried and the time it took place for diagnostics purposes.</p>
+                <p>We use <a href="https://plausible.io/">Plausible Analytics</a> to track basic use measurements, like the number of users.</p>
             </section>
         </div>
     );
