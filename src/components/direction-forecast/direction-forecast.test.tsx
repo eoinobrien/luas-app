9275feba7b9b082a0232cbc501466f2310b563a3
Eoin O'Brien <eoobrien@microsoft.com>
2022-11-29 00:24:38 -0500

    remove useless tests

--- src/components/direction-forecast/direction-forecast.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import DirectionForecast from './direction-forecast';
-import OperatingHoursModel from '../../models/OperatingHoursModel';
-
-test('renders without crashing', () => {
-    true
-});
